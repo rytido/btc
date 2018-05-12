d15b911f1f0ae2573fedc4ed0302ccb910b8ad1a
ryan <rtdood@mtu.edu>
2018-05-11 23:08:14 -0400

    intial commit

---
+++ btc_status.py
@@ -0,0 +1,62 @@
+import os
+import json
+import requests
+from collections import Counter
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+
+def slacker(channel, text):
+    webhook_url = os.environ['SLACK']
+    slack_data = {"channel": channel, "text": text}
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    return response.text
+
+cnxn_str = "http://{}:{}@{}".format(os.environ['RPC_USER'], os.environ['RPC_PASSWORD'], os.environ['NODE_IP'])
+def engine(commands):
+        return AuthServiceProxy(cnxn_str).batch_(commands)
+
+def shift_fee(fee):
+        return round(float(fee * 100000), 1)
+
+def get_tx(txid):
+        return engine([["getrawtransaction", txid, True]])[0]
+
+def much_info():
+        commands = [["getpeerinfo"], ["getblockcount"], ["getmempoolinfo"], ["getbestblockhash"],
+                        ["estimatesmartfee", 1, "ECONOMICAL"], ["estimatesmartfee", 6, "ECONOMICAL"], ["estimatesmartfee", 72, "ECONOMICAL"]]
+        names = ["_".join(str(s) for s in c) for c in commands]
+        results = engine(commands)
+        kv = dict(zip(names, results))
+        block = engine([["getblock", kv["getbestblockhash"]]])[0]
+        peers = kv["getpeerinfo"]
+
+        cleaned = {
+        "peers":len(peers),
+        "blocks":kv["getblockcount"], #['blocks']
+        "block_tx":len(block['tx']),
+        "block_mb":round(block['size']/1000000, 4),
+        #"difficulty":round(float(kv["getblockchaininfo"]['difficulty']/1000000000000), 3),
+        "memsize_tx":kv["getmempoolinfo"]['size'],
+        "memsize_mb":round(kv["getmempoolinfo"]['bytes']/1000000, 2),
+        "feerate_01block":shift_fee(kv["estimatesmartfee_1_ECONOMICAL"]['feerate']),
+        "feerate_01hour":shift_fee(kv["estimatesmartfee_6_ECONOMICAL"]['feerate']),
+        "feerate_12hour":shift_fee(kv["estimatesmartfee_72_ECONOMICAL"]['feerate']),
+        }
+
+        peer_versions = Counter(['peer_' + p['subver'][1:].split(':')[0].lower() for p in peers])
+        cleaned = {**cleaned, **peer_versions}
+        return cleaned
+
+
+infostr = "\n".join("%s: %s" % (k, v) for k,v in sorted(much_info().items()))
+
+slacker('#alerts', infostr)
+
+"""
+blocktx = engine([["getblock", kv["getbestblockhash"], 2]])[0]['tx']
+outs = [t['vout'] for t in blocktx[1:]]
+max_tx_out = round(max([max([o['value'] for o in out]) for out in outs]),2)
+"""
+
---
+++ lnd_status.py
@@ -0,0 +1,78 @@
+import os
+import json
+import requests
+import rpc_pb2 as ln
+import rpc_pb2_grpc as lnrpc
+import grpc
+import codecs
+
+def slacker(channel, text):
+    webhook_url = os.environ['SLACK']
+    slack_data = {"channel": channel, "text": text}
+    response = requests.post(
+        webhook_url, data=json.dumps(slack_data),
+        headers={'Content-Type': 'application/json'}
+    )
+    return response.text
+
+def show_dict(data):
+    return "\n".join("%s: %s" % (k, v) for k,v in sorted(data.items()))
+
+def save_dict(data, filename):
+    json.dump(data, open(filename, 'w'))
+
+def load_dict(filename):
+    if os.path.exists(filename):
+        return json.load(open(filename))
+    else:
+        return {}
+
+# Due to updated ECDSA generated tls.cert we need to let gprc know that
+# we need to use that cipher suite otherwise there will be a handhsake
+# error when we communicate with the lnd rpc server.
+os.environ["GRPC_SSL_CIPHER_SUITES"] = 'HIGH+ECDSA'
+
+# Lnd cert is at ~/.lnd/tls.cert on Linux and
+# ~/Library/Application Support/Lnd/tls.cert on Mac
+cert = open(os.path.expanduser('~/.lnd/tls.cert'), 'rb').read()
+creds = grpc.ssl_channel_credentials(cert)
+#channel = grpc.secure_channel('localhost:10009', creds)
+#stub = lnrpc.LightningStub(channel)
+
+def metadata_callback(context, callback):
+    with open(os.path.expanduser('~/.lnd/admin.macaroon'), 'rb') as f:
+        macaroon_bytes = f.read()
+        macaroon = codecs.encode(macaroon_bytes, 'hex')
+        callback([('macaroon', macaroon)], None)
+
+
+# build ssl credentials using the cert the same as before
+cert_creds = grpc.ssl_channel_credentials(cert)
+
+# now build meta data credentials
+auth_creds = grpc.metadata_call_credentials(metadata_callback)
+
+# combine the cert credentials and the macaroon auth credentials
+# such that every call is properly encrypted and authenticated
+combined_creds = grpc.composite_channel_credentials(cert_creds, auth_creds)
+
+# finally pass in the combined credentials when creating a channel
+channel = grpc.secure_channel('localhost:10009', combined_creds)
+stub = lnrpc.LightningStub(channel)
+
+# now every call will be made with the macaroon already included
+info = stub.GetInfo(ln.GetInfoRequest())
+data = {}
+data['num_active_channels'] = info.num_active_channels
+data['num_peers'] = info.num_peers
+
+filename = 'lnd.json'
+
+saved_data = load_dict(filename)
+
+if data != saved_data:
+    save_dict(data, filename)
+    network_info = stub.GetNetworkInfo(ln.NetworkInfoRequest())
+    data['num_nodes'] = network_info.num_nodes
+    data['num_channels'] = network_info.num_channels
+    slacker('#lnd', show_dict(data))
