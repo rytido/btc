fd74ed2e1deb8c6de37805268977ed65457a7d5d
ryan <rtdood@mtu.edu>
2018-09-21 20:28:01 -0400

    removed unecessary code and comments

--- lnd_status.py
+++ lnd_status.py
@@ -32,12 +32,7 @@ def load_dict(filename):
 # error when we communicate with the lnd rpc server.
 os.environ["GRPC_SSL_CIPHER_SUITES"] = 'HIGH+ECDSA'
 
-# Lnd cert is at ~/.lnd/tls.cert on Linux and
-# ~/Library/Application Support/Lnd/tls.cert on Mac
 cert = open(os.path.expanduser('~/.lnd/tls.cert'), 'rb').read()
-creds = grpc.ssl_channel_credentials(cert)
-#channel = grpc.secure_channel('localhost:10009', creds)
-#stub = lnrpc.LightningStub(channel)
 
 def metadata_callback(context, callback):
     with open(os.path.expanduser('~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon'), 'rb') as f:
