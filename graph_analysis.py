ad95ba5aa7c3c9924e380ccb914732dd3e254d20
ryan <rtdood@mtu.edu>
2019-01-02 22:22:44 -0500

    formatting

--- graph_analysis.py
+++ graph_analysis.py
@@ -2,8 +2,10 @@ import json
 import numpy as np
 import networkx as nx
 
+
 data = json.load(open('lightning_graph.json'))
 pubkey = open("pubkey.txt").read().strip()
+
 
 def get_nodes(data):
     nodes = {}
@@ -13,8 +15,10 @@ def get_nodes(data):
         nodes[d[1]] = nodes.get(d[1], 0) + w
     return nodes
 
+
 nodes = get_nodes(data)
 print("node count: {}".format(len(nodes)))
+
 
 def compute_score(paths, nodes):
     hop_data = []
@@ -25,16 +29,16 @@ def compute_score(paths, nodes):
             hop_data.append(np.log(capacity) / hops)
     return sum(hop_data) / len(hop_data)
 
+
 def make_graph(id=None):
     graph = nx.Graph()
     graph.add_edges_from(data)
     if id is not None:
-        graph.add_edges_from([(pubkey, id, {"weight":1000})])
+        graph.add_edges_from([(pubkey, id, {"weight": 1000})])
     paths = nx.shortest_path(graph, source=pubkey)
     score = compute_score(paths, nodes)
     hop_distribution = np.bincount([len(p)-1 for p in paths.values()])
     return score, hop_distribution
-
 
 
 current_score, hop_distribution = make_graph()
@@ -42,8 +46,8 @@ print(current_score, hop_distribution)
 
 onions = json.load(open("onions.json"))
 
-#for id in nodes.keys():
-for id in onions:
+for id in nodes.keys():
+#for id in onions:
     new_score, hop_distribution = make_graph(id)
     if new_score > current_score:
         print(new_score, hop_distribution, id)
--- lnd_graph.py
+++ lnd_graph.py
@@ -5,17 +5,19 @@ import codecs
 import rpc_pb2 as ln
 import rpc_pb2_grpc as lnrpc
 import grpc
-import pandas as pd
+
 
 MESSAGE_SIZE_MB = 50 * 1024 * 1024
-
 os.environ["GRPC_SSL_CIPHER_SUITES"] = 'HIGH+ECDSA'
 
+
 def metadata_callback(context, callback):
-    with open(os.path.expanduser('~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon'), 'rb') as f:
+    mac_file = '~/.lnd/data/chain/bitcoin/mainnet/admin.macaroon'
+    with open(os.path.expanduser(mac_file), 'rb') as f:
         macaroon_bytes = f.read()
         macaroon = codecs.encode(macaroon_bytes, 'hex')
         callback([('macaroon', macaroon)], None)
+
 
 def get_creds():
     cert = open(os.path.expanduser('~/.lnd/tls.cert'), 'rb').read()
@@ -27,12 +29,14 @@ def get_creds():
     combined_creds = grpc.composite_channel_credentials(cert_creds, auth_creds)
     return combined_creds
 
+
 def is_onion(stub, pubkey):
     nodeinfo = stub.GetNodeInfo(ln.NodeInfoRequest(pub_key=pubkey))
     if nodeinfo.node.addresses:
         return '.onion' in nodeinfo.node.addresses[0].addr
     else:
         return False
+
 
 channel_options = [
     ('grpc.max_message_length', MESSAGE_SIZE_MB),
@@ -43,7 +47,6 @@ channel = grpc.secure_channel('localhost:10009', get_creds(), channel_options)
 stub = lnrpc.LightningStub(channel)
 
 identity_pubkey = stub.GetInfo(ln.GetInfoRequest()).identity_pubkey
-#print(identity_pubkey)
 
 lgraph = stub.DescribeGraph(ln.ChannelGraphRequest())
 edges = lgraph.edges
